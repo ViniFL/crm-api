13cf85ddf5e406cfe137eae7baf477e19aec0699
Vinicius Fernandes <vinicius.fernandes.lima@outlook.com>
2023-10-04 23:19:50 -0300

    Handlers, custom errors

--- src/main/java/com/vinicius/salescontrol/controllers/ContactController.java
+++ src/main/java/com/vinicius/salescontrol/controllers/ContactController.java
@@ -1,4 +1,25 @@
 package com.vinicius.salescontrol.controllers;
 
+import com.vinicius.salescontrol.dto.ContactDTO;
+import com.vinicius.salescontrol.services.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/contacts")
 public class ContactController {
+
+    @Autowired
+    ContactService service;
+
+    @GetMapping
+    public ResponseEntity<Page<ContactDTO>> findAll(Pageable pageable) {
+        Page<ContactDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
 }
---
+++ src/main/java/com/vinicius/salescontrol/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.vinicius.salescontrol.controllers.handlers;
+
+import com.vinicius.salescontrol.dto.CustomError;
+import com.vinicius.salescontrol.dto.ValidationError;
+import com.vinicius.salescontrol.services.exceptions.DatabaseException;
+import com.vinicius.salescontrol.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+        for (FieldError f: e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+        return ResponseEntity.status(status).body(err);
+    }
+}
