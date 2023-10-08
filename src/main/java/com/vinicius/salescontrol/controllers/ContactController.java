f05a9053cf88d8b7d2d1f4869dde4014e1201df7
Vinicius Fernandes <vinicius.fernandes.lima@outlook.com>
2023-10-07 22:29:32 -0300

    Contact controller

--- src/main/java/com/vinicius/salescontrol/controllers/ContactController.java
+++ src/main/java/com/vinicius/salescontrol/controllers/ContactController.java
@@ -2,13 +2,15 @@ package com.vinicius.salescontrol.controllers;
 
 import com.vinicius.salescontrol.dto.ContactDTO;
 import com.vinicius.salescontrol.services.ContactService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/contacts")
@@ -21,5 +23,30 @@ public class ContactController {
     public ResponseEntity<Page<ContactDTO>> findAll(Pageable pageable) {
         Page<ContactDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ContactDTO> findById(@PathVariable Long id) {
+        ContactDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ContactDTO> insert(@Valid @RequestBody ContactDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ContactDTO> update(@PathVariable Long id, @Valid @RequestBody ContactDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
